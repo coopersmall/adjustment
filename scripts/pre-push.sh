65946e52175f02a414735321235031f09b1c0f25
Cooper <cooper@coopcommit.com>
2023-06-15 20:44:27 -0600

    update script again...

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -78,7 +78,7 @@ if [ "$first_push" = true ]; then
     done
 
     # Increase the patch version for the workspace if it has changes
-    if ! git log --oneline --name-only --diff-filter=ACMRTUXB "$(git merge-base origin/master HEAD)..HEAD" -- . | grep -q "Bump"; then
+    if ! git log --oneline --name-only --diff-filter=ACMRTUXB "$(git merge-base origin/master HEAD)..HEAD" -- . | grep -q '^'; then
         workspace_version=$(awk -F'"' '/version =/{print $2}' Cargo.toml)
         major="${workspace_version%%.*}"
         minor="${workspace_version#*.}"
@@ -102,12 +102,11 @@ else
         crate_version=$(awk -F'"' '/version =/{print $2}' "${crate}/Cargo.toml")
         if ! git diff --name-only --diff-filter=ACMRTUXB "$(git merge-base origin/master HEAD)" -- "${crate}/" | grep -q '^'; then
             master_version=$(git show "origin/master:${crate}/Cargo.toml" | awk -F'"' '/version =/{print $2}')
-            if version_compare "$crate_version" "$master_version" && [[ $? -le 2 ]]; then
+            if version_compare "$crate_version" "$master_version" && [[ $? -eq 2 ]]; then
                 major="${crate_version%%.*}"
                 minor="${crate_version#*.}"
                 patch="${minor#*.}"
-                new_patch=$(awk -F'.' '{print $3}' <<< "$master_version")
-                new_version="${major}.${minor}.${new_patch}"
+                new_version="${major}.${minor}.$((patch + 1))"
                 sed -i.bak "s/^version = \".*\"/version = \"${new_version}\"/" "${crate}/Cargo.toml"
                 echo "Bumped ${crate} version to ${new_version}"
                 git add "${crate}/Cargo.toml"
@@ -119,12 +118,11 @@ else
     workspace_version=$(awk -F'"' '/version =/{print $2}' Cargo.toml)
     if ! git diff --name-only --diff-filter=ACMRTUXB "$(git merge-base origin/master HEAD)" -- . | grep -q '^'; then
         master_workspace_version=$(git show "origin/master:Cargo.toml" | awk -F'"' '/version =/{print $2}')
-        if version_compare "$workspace_version" "$master_workspace_version" && [[ $? -le 2 ]]; then
+        if version_compare "$workspace_version" "$master_workspace_version" && [[ $? -eq 2 ]]; then
             major="${workspace_version%%.*}"
             minor="${workspace_version#*.}"
             patch="${minor#*.}"
-            new_patch=$(awk -F'.' '{print $3}' <<< "$master_workspace_version")
-            new_version="${major}.${minor}.${new_patch}"
+            new_version="${major}.${minor}.$((patch + 1))"
             sed -i.bak "s/^version = \".*\"/version = \"${new_version}\"/" Cargo.toml
             echo "Bumped workspace version to ${new_version}"
             git add Cargo.toml
