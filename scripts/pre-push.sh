71612ab7e23bc39181e28f57a66ecd6440726198
Cooper <cooper@coopcommit.com>
2023-06-15 18:35:14 -0600

    changes

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,73 +1,3 @@
-#!/bin/bash
-
-# Function to check if a command is available
-command_exists() {
-    command -v "$1" >/dev/null 2>&1
-}
-
-# Function to compare versions
-version_compare() {
-    local latest_version="$1"
-    local current_version="$2"
-    local major_latest="${latest_version%%.*}"
-    local major_current="${current_version%%.*}"
-    local minor_latest="${latest_version#*.}"
-    local minor_current="${current_version#*.}"
-    local patch_latest="${minor_latest#*.}"
-    local patch_current="${minor_current#*.}"
-
-    if [[ $major_current -lt $major_latest ]]; then
-        return 1
-    elif [[ $major_current -gt $major_latest ]]; then
-        return 2
-    elif [[ $minor_current -lt $minor_latest ]]; then
-        return 1
-    elif [[ $minor_current -gt $minor_latest ]]; then
-        return 2
-    elif [[ $patch_current -lt $patch_latest ]]; then
-        return 1
-    elif [[ $patch_current -gt $patch_latest ]]; then
-        return 2
-    else
-        return 0
-    fi
-}
-
-# Check if cargo is installed
-if ! command_exists cargo; then
-    echo "cargo not found. Installing cargo..."
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-    source $HOME/.cargo/env
-fi
-
-# Check if rustc is installed
-if ! command_exists rustc; then
-    echo "rustc not found. Installing Rust..."
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-    source $HOME/.cargo/env
-fi
-
-# Check if cargo-fmt is installed
-if ! command_exists cargo-fmt; then
-    echo "cargo-fmt not found. Installing cargo-fmt..."
-    cargo install --force cargo-fmt
-fi
-
-# Check if cargo-clippy is installed
-if ! command_exists cargo-clippy; then
-    echo "cargo-clippy not found. Installing cargo-clippy..."
-    cargo install --force cargo-clippy
-fi
-
-# Get the branch name
-branch=$(git symbolic-ref --short HEAD)
-
-# Determine if this is the first push to origin
-first_push=false
-if ! git rev-parse --abbrev-ref "@{u}" >/dev/null 2>&1; then
-    first_push=true
-fi
-
 # Update crate versions if changes were made
 if [ "$first_push" = true ]; then
     echo "First push to origin. Bumping versions for all crates."
@@ -76,8 +6,7 @@ if [ "$first_push" = true ]; then
     # Increase the patch version for each crate with changes
     for crate in "utils" "common" "macros"; do
         if git diff --name-only --diff-filter=ACMRTUXB "origin/master" -- "${crate}/"; then
-            master_version=$(git show "origin/master:./Cargo.toml" | awk -F'"' '/version =/{print $2; exit}')
-
+            crate_version=$(awk -F'"' '/version =/{print $2}' "${crate}/Cargo.toml")
             major="${crate_version%%.*}"
             minor="${crate_version#*.}"
             patch="${minor#*.}"
@@ -90,7 +19,7 @@ if [ "$first_push" = true ]; then
 
     # Increase the patch version for the workspace if it has changes
     if git diff --name-only --diff-filter=ACMRTUXB "origin/master" -- .; then
-        workspace_version=$(grep -Po '(?<=version = ")[^"]+' Cargo.toml)
+        workspace_version=$(awk -F'"' '/version =/{print $2}' Cargo.toml)
         major="${workspace_version%%.*}"
         minor="${workspace_version#*.}"
         patch="${minor#*.}"
@@ -101,12 +30,12 @@ if [ "$first_push" = true ]; then
     fi
 else
     echo "Checking for version updates in changed crates..."
-    master_version=$(git show "origin/master:./Cargo.toml" | grep -Po '(?<=version = ")[^"]+' | head -n1)
+    master_version=$(git show "origin/master:./Cargo.toml" | awk -F'"' '/version =/{print $2; exit}')
 
     # Compare and update versions for changed crates
     for crate in "utils" "common" "macros"; do
         if git diff --name-only --diff-filter=ACMRTUXB "origin/master" -- "${crate}/"; then
-            crate_version=$(grep -Po '(?<=version = ")[^"]+' "${crate}/Cargo.toml")
+            crate_version=$(awk -F'"' '/version =/{print $2}' "${crate}/Cargo.toml")
             version_compare "$master_version" "$crate_version"
             case $? in
                 1)
@@ -131,7 +60,7 @@ else
 
     # Compare and update version for the workspace if it has changes
     if git diff --name-only --diff-filter=ACMRTUXB "origin/master" -- .; then
-        workspace_version=$(grep -Po '(?<=version = ")[^"]+' Cargo.toml)
+        workspace_version=$(awk -F'"' '/version =/{print $2}' Cargo.toml)
         version_compare "$master_version" "$workspace_version"
         case $? in
             1)
@@ -152,35 +81,4 @@ else
                 ;;
         esac
     fi
-fi
-
-# Run cargo build
-echo "Running cargo build..."
-cargo build
-
-# Run cargo test
-echo "Running cargo test..."
-cargo test
-
-# Run cargo fmt
-echo "Running cargo fmt..."
-cargo fmt --all -- --check
-
-# Run cargo clippy
-# echo "Running cargo clippy..."
-# cargo clippy --all-targets -- -D warnings
-
-# If any of the commands above fail, exit with a non-zero status
-if [ $? -ne 0 ]; then
-    echo "Pre-push checks failed. Please fix the errors before pushing."
-    exit 1
-fi
-
-# Check if any changes were made to crate versions
-if git diff --quiet --exit-code; then
-    echo "No crate version changes detected."
-else
-    echo "Committing crate version changes."
-    git add . && git commit -m "Bump crate versions"
-    exec git push origin "$branch"
 fi
