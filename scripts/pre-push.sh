ad4e3e074229083de4d6ae1ffac07dc4fd72dcc5
Cooper <cooper@coopcommit.com>
2023-06-17 07:29:09 -0600

    test

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -11,127 +11,93 @@ yellow=$(tput setaf 228)
 bright_red=$(tput setaf 196)
 reset=$(tput sgr0)
 
-compare_major_versions() {
+compare_versions() {
     if [[ $1 == $2 ]]; then
         return 0
     fi
 
-    IFS='.' read -ra v1 <<< "$1"
-    IFS='.' read -ra v2 <<< "$2"
-
-    # Compare the major version with master
-    if ((10#${v1[0]:-0} == 10#${v2[0]:-0})); then
-        # The major version is the same as master
-        return 0
-    elif ((10#${v1[0]:-0} < 10#${v2[0]:-0})); then
-        # The major version is less than the master version
-        return 1
-    else
-        # The major version is greater than the master version
-        return 2
-    fi
-
-    return 0
-}
-
-compare_minor_version() {
-    if [[ $1 == $2 ]]; then
-        return 0
-    fi
+    version_type=$3
 
     IFS='.' read -ra v1 <<< "$1"
     IFS='.' read -ra v2 <<< "$2"
 
-    # Compare the minor version with master
-    if ((10#${v1[1]:-0} == 10#${v2[1]:-0})); then
-        # The minor version is the same as master
-        return 0
-    elif ((10#${v1[1]:-0} < 10#${v2[1]:-0})); then
-        # The minor version is less than the master version
-        return 1
-    else
-        # The minor version is greater than the master version
-        return 2
+    if [[ version_type == "major" ]]; then
+        # Compare the major version with master
+        if ((10#${v1[0]} == 10#${v2[0]})); then
+            # The major version is the same as master
+            return 0
+        elif ((10#${v1[0]} < 10#${v2[0]})); then
+            # The major version is less than the master version
+            return 1
+        else
+            # The major version is greater than the master version
+            return 2
+        fi
     fi
 
-    return 0
+    if [[ version_type == "minor" ]]; then
+        # Compare the minor version with master
+        if ((10#${v1[1]:-0} == 10#${v2[1]:-0})); then
+            # The minor version is the same as master
+            return 0
+        elif ((10#${v1[1]:-0} < 10#${v2[1]:-0})); then
+            # The minor version is less than the master version
+            return 1
+        else
+            # The minor version is greater than the master version
+            return 2
+        fi
+    fi
+
+    if [[ version_type == "patch" ]]; then
+        # Compare the patch version with master
+        if ((10#${v1[2]:-0} == 10#${v2[2]:-0})); then
+            # The patch version is the same as master
+            return 0
+        elif ((10#${v1[2]:-0} < 10#${v2[2]:-0})); then
+            # The patch version is less than the master version
+            return 1
+        else
+            # The patch version is greater than the master version
+            return 2
+        fi
+    fi
 }
 
-compare_patch_versions() {
-    if [[ $1 == $2 ]]; then
-        return 0
-    fi
-
-    IFS='.' read -ra v1 <<< "$1"
-    IFS='.' read -ra v2 <<< "$2"
-
-    # Compare the patch versions
-    if ((10#${v1[2]:-0} == 10#${v2[2]:-0})); then
-        # The patch version is the same as master
-        return 0
-    elif ((10#${v1[2]:-0} < 10#${v2[2]:-0})); then
-        # The patch version is less than the master version
-        return 1
-    else
-        # The patch version is greater than the master version
-        return 2
-    fi
-
-    return 0
-}
-
-was_major_version_increased_by_one() {
+was_version_bumped() {
     current_version=$1
     master_version=$2
+    version_type=$3
 
     IFS='.' read -ra current <<< "$current_version"
     IFS='.' read -ra master <<< "$master_version"
 
-    # Compare the major version with master to see if it was increased by one and the minor and patch versions are 0 (i.e. 1.0.0 -> 2.0.0)
-    if ((10#${current[0]} == 10#${master[0]} + 1)) && ((10#${current[1]} == 0)) && ((10#${current[2]} == 0)); then
-        return 0
-    else
-        return 1
-    fi
-}
-
-was_minor_version_increased_by_one() {
-    current_version=$1
-    master_version=$2
-
-    IFS='.' read -ra current <<< "$current_version"
-    IFS='.' read -ra master <<< "$master_version"
-
-    # Compare the minor version with master to see if it was increased by one and the patch version is 0 (i.e. 1.1.0 -> 1.2.0)
-    if ((10#${current[0]} == 10#${master[0]})) && ((10#${current[1]} == 10#${master[1]} + 1)) && ((10#${current[2]} == 0)); then
-        return 0
-    else
-        return 1
-    fi
-}
-
-# Function to validate if the only change is to the Cargo.toml file
-was_cargo_toml_only_change() {
-    local file="$1"
-
-    # Get the commit history for the branch
-    commit_history=$(git log --name-only --oneline "${file}")
-
-    # Split the commit history by newlines
-    IFS=$'\n' read -rd '' -a commits <<< "$commit_history"
-
-    # Check if there is only one commit in the history
-    if [[ ${#commits[@]} -eq 1 ]]; then
-        # Get the file changes in the commit
-        changed_files=$(git diff --name-only "${commits[0]}")
-
-       # Check if the only changed file is the Cargo.toml for the given crate,
-       if [[ $changed_files == "${file}" ]] && [[ $(awk -F'"' '/^\[package\]/ { package = 1 } package && /^version *=/ { gsub(/^[[:space:]]+|"[[:space:]]+$/, "", $2); print $2; exit }' "${file}") ]]; then
-           return 0
-       fi 
+    if [[ $version_type == "major" ]]; then
+        # Compare the major version with master to see if it was increased by one and the minor and patch versions are 0 (i.e. 1.0.0 -> 2.0.0)
+        if ((10#${current[0]} == 10#${master[0]} + 1)) && ((10#${current[1]} == 0)) && ((10#${current[2]} == 0)); then
+            return 0
+        else
+            return 1
+        fi
     fi
 
-    return 1
+    if [[ $version_type == "minor" ]]; then
+        # Compare the minor version with master to see if it was increased by one and the patch version is 0 (i.e. 1.1.0 -> 1.2.0)
+        if ((10#${current[0]} == 10#${master[0]})) && ((10#${current[1]} == 10#${master[1]} + 1)) && ((10#${current[2]} == 0)); then
+            return 0
+        else
+            return 1
+        fi
+    fi
+
+    if [[ $version_type == "patch" ]]; then
+        # Compare the patch version with master to see if it was increased by one (i.e. 1.1.1 -> 1.1.2)
+        if ((10#${current[0]} == 10#${master[0]})) && ((10#${current[1]} == 10#${master[1]})) && ((10#${current[2]} == 10#${master[2]} + 1)); then
+            return 0
+        else
+            return 1
+        fi
+    fi
 }
 
 bump_version() {
@@ -173,6 +139,30 @@ bump_version() {
     fi
 
     echo "${new_version}"
+}
+
+# Function to validate if the only change is to the Cargo.toml file
+check_toml_file() {
+    local file="$1"
+
+    # Get the commit history for the branch
+    commit_history=$(git log --name-only --oneline "${file}")
+
+    # Split the commit history by newlines
+    IFS=$'\n' read -rd '' -a commits <<< "$commit_history"
+
+    # Check if there is only one commit in the history
+    if [[ ${#commits[@]} -eq 1 ]]; then
+        # Get the file changes in the commit
+        changed_files=$(git diff --name-only "${commits[0]}")
+
+       # Check if the only changed file is the Cargo.toml for the given crate,
+       if [[ $changed_files == "${file}" ]] && [[ $(awk -F'"' '/^\[package\]/ { package = 1 } package && /^version *=/ { gsub(/^[[:space:]]+|"[[:space:]]+$/, "", $2); print $2; exit }' "${file}") ]]; then
+           return 0
+       fi 
+    fi
+
+    return 1
 }
 
 is_first_push() {
@@ -223,55 +213,53 @@ for crate in "${crate_names[@]}"; do
     was_major_version_changed=false
     was_minor_version_changed=false
 
-    echo "$(compare_major_versions "$crate_version" "$master_version")"
-
     # Compare the crate major version with the master version and update if necessary
-    if compare_major_versions "$crate_version" "$master_version" && [[ $? -eq 2 ]]; then
+    if compare_versions "$crate_version" "$master_version" "major" && [[ $? -eq 2 ]]; then
         echo "${yellow}Major version change detected in commit history. Validating version change...${reset}"
         was_major_version_changed=true
 
         # Validate that the major version was increased only by 1
-        if ! was_major_version_increased_by_one "$crate_version" "$master_version"; then
+        if ! was_version_bumped "$crate_version" "$master_version" "major"; then
             echo
             echo "${bright_red}Unable to validate major version change. Please ensure that the only change in the commit for your branch is to the version variable in the ${toml_path} file.${reset}"
             exit 1
         fi
 
         # Validate if the only change in the commit is to the version variable in the Cargo.toml file
-        if was_cargo_toml_only_change "${toml_path}"; then
-            echo "Successfully validated major version change!"
-        else
+        if ! check_toml_file "${toml_path}"; then
             echo
             echo "${bright_red}Major version updates can only be done in increments of 1. Double check the Cargo.toml file for ${crate}.${reset}"
             exit 1
         fi
+
+            echo "Successfully validated major version change!"
     fi
 
     # Compare the crate minor version with the master version and update if necessary
-    if compare_minor_version "$crate_version" "$master_version" && [[ $? -eq 2 ]]; then
+    if compare_versions "$crate_version" "$master_version" "minor" && [[ $? -eq 2 ]]; then
         echo "${yellow}Minor version change detected in commit history. Validating version change...${reset}"
         was_minor_version_changed=true
 
         # Validate that the minor version was increased only by 1
-        if ! was_minor_version_increased_by_one "$crate_version" "$master_version"; then
+        if ! was_version_bumped "$crate_version" "$master_version" "minor"; then
             echo
             echo "${bright_red}Minor version updates can only be done in increments of 1. Double check the Cargo.toml file for ${crate}.${reset}"
             exit 1
         fi
 
         # Validate if the only change in the commit is to the version variable in the Cargo.toml file
-        if was_cargo_toml_only_change "${toml_path}"; then
-            echo "${light_green}Successfully validated minor version change!${reset}"
-        else
+        if ! check_toml_file "${toml_path}"; then
             echo
             echo "${bright_red}Unable to validate minor version change. Please ensure that the only change in the commit for your branch is to the version variable in the ${crate}/Cargo.toml file.${reset}"
             exit 1
         fi
+
+            echo "${light_green}Successfully validated minor version change!${reset}"
     fi
 
     # Compare the crate version with the master version and update if necessary
     # If the major or minor version was changed, then the patch version is not checked
-    if compare_patch_versions "$crate_version" "$master_version" && [[ $? -le 1 ]] && ! $was_major_version_changed && ! $was_minor_version_changed; then
+    if compare_versions "$crate_version" "$master_version" "patch" && [[ $? -le 1 ]] && ! $was_major_version_changed && ! $was_minor_version_changed; then
         # Extract major, minor, and patch versions using regex and validate them
         echo "${yellow}Version bump required. Bumping version...${reset}"
 
