a10e3bd5e189a494cf5a28a83e9d01af1c218a1e
Cooper <cooper@coopcommit.com>
2023-06-26 07:47:53 -0600

    add documentation

--- utils/src/http.rs
+++ utils/src/http.rs
@@ -3,7 +3,6 @@ pub mod request;
 pub mod response;
 pub mod url;
 
-pub use request::HttpRequestBuilder;
-pub use request::{HttpMethod, HttpRequest};
+pub use request::{HttpMethod, HttpRequest, HttpRequestBuilder};
 pub use response::HttpResponse;
 pub use url::Url;
